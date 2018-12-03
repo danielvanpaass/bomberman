0a9f7e54d52127fb435cfcb2c6482f7e4369b18f
Harry <koen2mail@gmail.com>
2018-12-03 11:38:20 +0100

    Delete hitscan_top_structural_cfg.vhd
    Oudere versie

--- Rest_Main/hitscan_top_structural_cfg.vhd
+++
@@ -1,8 +0,0 @@
-configuration hitscan_top_structural_cfg of hitscan_top is
-   for structural
-      for all: ff2 use configuration work.ff2_behaviour_cfg;
-      end for;
-      for all: hitscan use configuration work.hitscan_behaviour_cfg;
-      end for;
-   end for;
-end hitscan_top_structural_cfg;
