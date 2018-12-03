f2eadd72dd4e7598d56554dd268ca7cc42395413
Harry <koen2mail@gmail.com>
2018-12-03 11:08:44 +0100

    Add files via upload
    Alle VHDL files van mij

---
+++ Rest_Main/hitscan_top.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+
+entity hitscan_top is
+   port(X_b   : IN std_logic_vector(3 downto 0);
+	Y_b   : IN std_logic_vector(3 downto 0);
+        res   : IN		std_logic;
+	clk   : IN  std_logic;
+	explode : IN std_logic;
+	X_p1  : IN  std_logic_vector(3 downto 0);
+        Y_p1  : IN  std_logic_vector(3 downto 0);
+        X_p2  : IN  std_logic_vector(3 downto 0);
+        Y_p2  : IN  std_logic_vector(3 downto 0);
+	victoryv   : OUT std_logic_vector(1 downto 0);
+        X_out : OUT std_logic_vector(3 downto 0);
+        Y_out : OUT std_logic_vector(3 downto 0));
+end hitscan_top;
+
