f0afe133d673a20c812a703aee491ba3135b41bc
danielvanpaass <38658984+danielvanpaass@users.noreply.github.com>
2018-12-05 20:38:13 +0100

    beautify

--- vhdl/hitbox/hitbox-behaviour.vhd
+++ vhdl/hitbox/hitbox-behaviour.vhd
@@ -1,268 +1,233 @@
-library IEEE;
-use IEEE.std_logic_1164.ALL;
-USE ieee.numeric_std.ALL; 
-
-
-architecture hitbox_behaviour of hitbox is
-type state_type is (begin_state, which_direction, attempt_to_right, attempt_to_left, attempt_to_up, attempt_to_down, right_output, left_output,up_output,down_output);
-type switch_type is (begin_state, P1, P2);
-SIGNAL dir_state, new_state: state_type;
-signal switch_state, new_switch_state: switch_type;
-signal new_x_player, new_y_player, x_player, y_player : STD_logic_vector (3 downto 0);
-signal check_x_player, check_y_player: std_logic_vector (3 downto 0); 
-signal hitbox_count_players: std_logic_vector (11 downto 0);
-signal start_hitbox_count_players, move_player, up_player, down_player, right_player, left_player, switch_players: std_logic;
-SIGNAL count_players,  new_count_players : unsigned (11 DOWNTO 0);
-begin
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+ARCHITECTURE hitbox_behaviour OF hitbox IS
+ TYPE state_type IS (begin_state, which_direction, attempt_to_right, attempt_to_left, attempt_to_up, attempt_to_down, right_output, left_output, up_output, down_output);
+ TYPE switch_type IS (begin_state, P1, P2);
+ SIGNAL dir_state, new_state                                                                                       : state_type;
+ SIGNAL switch_state, new_switch_state                                                                             : switch_type;
+ SIGNAL new_x_player, new_y_player, x_player, y_player                                                             : STD_logic_vector (3 DOWNTO 0);
+ SIGNAL check_x_player, check_y_player                                                                             : std_logic_vector (3 DOWNTO 0);
+ SIGNAL hitbox_count_players                                                                                       : std_logic_vector (11 DOWNTO 0);
+ SIGNAL start_hitbox_count_players, move_player, up_player, down_player, right_player, left_player, switch_players : std_logic;
+ SIGNAL count_players, new_count_players                                                                           : unsigned (11 DOWNTO 0);
+BEGIN
  PROCESS (clk)
-
  BEGIN
-  IF rising_edge (clk) THEN 
-
-     if reset = '1' then -- reset the whole system
-	dir_state <= begin_state;
-	switch_state<= begin_state;
-	count_players <= (OTHERS => '0');
-
-	else
-	dir_state <= new_state;
-	switch_state<= new_switch_state;
-	
-      END IF;
-      if start_hitbox_count_players='0' then
-	count_players <= (OTHERS => '0');
-	else
-	count_players <= new_count_players;
-	end if;
+  IF rising_edge (clk) THEN
+   IF reset = '1' THEN -- reset the whole system
+    dir_state     <= begin_state;
+    switch_state  <= begin_state;
+    count_players <= (OTHERS => '0');
+   ELSE
+    dir_state    <= new_state;
+    switch_state <= new_switch_state;
+   END IF;
+   IF start_hitbox_count_players = '0' THEN
+    count_players <= (OTHERS => '0');
+   ELSE
+    count_players <= new_count_players;
+   END IF;
   END IF;
-END PROCESS;
-
-
-
--- counter for P1 and P2 playtime
+ END PROCESS;
+ -- counter for P1 and P2 playtime
  PROCESS (count_players)
- BEGIN
-  new_count_players <= count_players + 1;
- END PROCESS;
- hitbox_count_players <= std_logic_vector (count_players);
-
---------- decides which player is to play
-PROCESS (switch_state, hitbox_count_players)
- BEGIN
-case switch_state is
-	when begin_state=>
-		switch_players <= '0';
-		x_player <= "0001";
-		y_player <= "0001";
-		x_p1 <= "0001";
-		y_p1 <= "0001";
-		x_p2 <= "1001";
-		y_p2 <= "1001";
-		new_switch_state <= P1;
-		start_hitbox_count_players <= '0';
-		up_player <= '0';
-		left_player <= '0';
-		right_player <= '0';
-		down_player <= '0';
-
-	when P1=>
-		if (hitbox_count_players = "000000110100" OR (hitbox_count_players > "000001110100")) then
-		switch_players <= '1';
-		else
-		switch_players <= '0';
-		end if;
-		x_player <= x_p1;
-		y_player <= y_p1;
-		start_hitbox_count_players <= '1';
-		up_player <= up_p1;
-		left_player <= left_p1;
-		right_player <= right_p1;
-		down_player <= down_p1;
-
-		if ((hitbox_count_players = "000000110100")) then --P1 ends his turn
-    		new_switch_state <= P2;
-		x_p1 <= new_x_player; -- output the new location for P1
-		y_p1 <= new_y_player;
-  		else
-		new_switch_state <= P1;
-		end if;
-	
-	when P2 =>
-		if (hitbox_count_players = "000000110100" OR (hitbox_count_players > "000001110100")) then
-		switch_players <= '1';
-		else
-		switch_players <= '0';
-		end if;
-		--p1_turn <= '0';-- p2 is active
-		start_hitbox_count_players <= '1';
-		x_player <= x_p2;
-		y_player <= y_p2;
-		up_player <= up_p2;
-		left_player <= left_p2;
-		right_player <= right_p2;
-		down_player <= down_p2;
-		if (hitbox_count_players > "000001110100") then -- should be the above but doubled, for the reset
-		start_hitbox_count_players <= '0';--the reset
-		new_switch_state<= P1;
-    		x_p2<= new_x_player; -- output the new location for P2
-		y_p2 <= new_y_player;-- or new_?
-		else
-		new_switch_state<= P2;
-		end if;
-
-end case;
-
- END PROCESS;
---------------
-
- process(right_player,left_player, up_player, down_player, dir_state, y_player,  x_player, hitbox_count_players, move_player)
-begin
-
-case dir_state is
-	when begin_state=>
-	new_state <= which_direction;
-	new_x_player<=  x_player;
-	new_y_player<= y_player;
-	check_x_player <= "0000";--- could these be removed? the output of this isnt important at this state
-	check_y_player <= "0000";
-
-
-	when which_direction=>
-	check_x_player <= "0000";
-	check_y_player <= "0000";
-	if (hitbox_count_players = "000000110100" OR (hitbox_count_players > "000001110100")) then--- so dont go to attempt state if new player is inserted in fsm
-		new_state <= which_direction;
-	else
-		if ((down_player = '0') AND (up_player = '0') AND (left_player = '0') AND (right_player = '1'))then  ---maybe change this back to priority case
-		new_state <= attempt_to_right;
-		elsif ((down_player = '0') AND (up_player = '0') AND (left_player = '1') AND (right_player = '0')) then
-		new_state <= attempt_to_left;
-
-		elsif ((down_player = '0') AND (up_player = '1') AND (left_player = '0') AND (right_player = '0')) then
-			new_state <= attempt_to_up;
-
-		elsif ((down_player = '1') AND (up_player = '0') AND (left_player = '0') AND (right_player = '0')) then
-		new_state <= attempt_to_down;
-		else
-		check_x_player <= "0000";
-		check_y_player <= "0000";
-		new_state <= which_direction;
-		end if;
-	end if;
-
-
-	--attempt states
-	when attempt_to_right =>
-		new_x_player<=  x_player;
-		new_y_player<= y_player;
-		check_x_player <= std_logic_vector(unsigned( x_player)+"0001");
-		check_y_player <= (y_player);
-		if( move_player = '1' ) then
-			new_state <= right_output;
-		else 
-			new_state <= which_direction;
-		end if;
-		
-	when attempt_to_left =>
-		new_x_player<=  x_player;
-		new_y_player<= y_player;
-		check_x_player <= std_logic_vector(unsigned( x_player)-1);
-		check_y_player <= (y_player);
-		if( move_player = '1') then
-			new_state <= left_output;
-		else 
-			new_state <= which_direction;
-		end if;
-
-
-	when attempt_to_up =>
-		new_x_player<=  x_player;
-		new_y_player<= y_player;
-		check_x_player <= ( x_player);
-		check_y_player <= std_logic_vector(unsigned(y_player)-1);
-		if( move_player = '1') then
-			new_state <= up_output;
-		else 
-			new_state <= which_direction;
-		end if;
-
-	when attempt_to_down =>
-		new_x_player<=  x_player;
-		new_y_player<= y_player;
-		check_x_player <= ( x_player);
-		check_y_player <= std_logic_vector(unsigned(y_player)+1);
-		if( move_player = '1') then
-			new_state <= down_output;
-		else 
-			new_state <= which_direction;
-		end if;
-
---- output states
-	when right_output =>
-		new_x_player <= std_logic_vector(unsigned( x_player)+"0001");
-		new_y_player <= y_player;
-		check_x_player <= "0000";
-		check_y_player <= "0000";
-		if (switch_players = '1') then -- 
-			new_state<=which_direction;
-		else
-			new_state<= right_output;
-		end if;
-		
-	when left_output =>
-		new_x_player <= std_logic_vector(unsigned( x_player)-"0001");
-		new_y_player <= y_player;
-		check_x_player <= "0000";
-		check_y_player <= "0000";
-		if (switch_players = '1') then -- 
-			new_state<=which_direction;
-		else
-			new_state<= left_output;
-		end if;
-	when up_output =>
-		new_x_player <= x_player;
-		new_y_player <= std_logic_vector(unsigned( y_player)-"0001");
-		check_x_player <= "0000";
-		check_y_player <= "0000";
-		if (switch_players = '1') then -- 
-			new_state<=which_direction;
-		else
-			new_state<= up_output;
-		end if;
-	when down_output =>
-		new_x_player <= x_player;
-		new_y_player <= std_logic_vector(unsigned( y_player)+"0001");
-		check_x_player <= "0000";
-		check_y_player <= "0000";
-		if (switch_players = '1') then -- this signal changes when P2 goes to one or 
-			new_state<=which_direction;
-		else
-			new_state<= down_output;
-		end if;
-	
-END CASE;
-
-end process;
- 
-------------- Check if there's an obstacle module for P1 (might be a problem that this doesnt update on clock)
-process (clk,  walls_and_crates, check_x_player, check_y_player, bomb_x_a, bomb_y_a, bomb_a_active, bomb_x_b, bomb_y_b, bomb_b_active)---bomb_x_c, bomb_y_c, bomb_c_active,bomb_x_d, bomb_y_d, bomb_d_active,bomb_x_e, bomb_y_e, bomb_e_active,bomb_x_f, bomb_y_f, bomb_f_active,bomb_x_g, bomb_y_g, bomb_g_active,bomb_x_h, bomb_y_h, bomb_h_active
-begin
-	if (  
-		(   walls_and_crates(  to_integer(unsigned(check_x_player))+to_integer(unsigned(check_y_player))*11) = '0')
-	AND (    bomb_x_a /= std_logic_vector(check_x_player) OR (bomb_y_a /= std_logic_vector(check_y_player)) OR ( bomb_a_active = '0' ) )
-	AND (    bomb_x_b /= std_logic_vector(check_x_player) OR (bomb_y_b /= std_logic_vector(check_y_player)) OR( bomb_b_active = '0' ) )
-	--AND (    bomb_x_c /= std_logic_vector(check_x_player) OR (bomb_y_c /= std_logic_vector(check_y_player)) OR( bomb_c_active = '0' ) )
-	--AND (    bomb_x_d /= std_logic_vector(check_x_player) OR (bomb_y_d /= std_logic_vector(check_y_player)) OR( bomb_d_active = '0' ) )
-	--AND (    bomb_x_e /= std_logic_vector(check_x_player) OR (bomb_y_e /= std_logic_vector(check_y_player)) OR( bomb_e_active = '0' ) )
-	--AND (    bomb_x_f /= std_logic_vector(check_x_player) OR (bomb_y_f /= std_logic_vector(check_y_player)) OR( bomb_f_active = '0' ) )
-	--AND (    bomb_x_g /= std_logic_vector(check_x_player) OR (bomb_y_g /= std_logic_vector(check_y_player)) OR( bomb_g_active = '0' ) )
-	--AND (    bomb_x_h /= std_logic_vector(check_x_player) OR (bomb_y_h /= std_logic_vector(check_y_player)) OR( bomb_h_active = '0' ) )
-	) then
-		move_player <= '1';
-	else
-		move_player <= '0';
-	end if;
-end process;
-
-end hitbox_behaviour;
-
+  BEGIN
+   new_count_players <= count_players + 1;
+  END PROCESS;
+  hitbox_count_players <= std_logic_vector (count_players);
+  --------- decides which player is to play
+  PROCESS (switch_state, hitbox_count_players)
+   BEGIN
+    CASE switch_state IS
+     WHEN begin_state =>
+      switch_players             <= '0';
+      x_player                   <= "0001";
+      y_player                   <= "0001";
+      x_p1                       <= "0001";
+      y_p1                       <= "0001";
+      x_p2                       <= "1001";
+      y_p2                       <= "1001";
+      new_switch_state           <= P1;
+      start_hitbox_count_players <= '0';
+      up_player                  <= '0';
+      left_player                <= '0';
+      right_player               <= '0';
+      down_player                <= '0';
+     WHEN P1 =>
+      IF (hitbox_count_players = "000000110100") THEN -- OR (hitbox_count_players > "000001110100" removed
+       switch_players <= '1';
+      ELSE
+       switch_players <= '0';
+      END IF;
+      x_player                   <= x_p1;
+      y_player                   <= y_p1;
+      start_hitbox_count_players <= '1';
+      up_player                  <= up_p1;
+      left_player                <= left_p1;
+      right_player               <= right_p1;
+      down_player                <= down_p1;
+      IF ((hitbox_count_players = "000000110100")) THEN --P1 ends his turn
+       new_switch_state <= P2;
+       x_p1             <= new_x_player; -- output the new location for P1
+       y_p1             <= new_y_player;
+      ELSE
+       new_switch_state <= P1;
+      END IF;
+     WHEN P2 =>
+      IF (hitbox_count_players = "000000110100") THEN -- OR (hitbox_count_players > "000001110100" removed
+       switch_players <= '1';
+      ELSE
+       switch_players <= '0';
+      END IF;
+      start_hitbox_count_players <= '1';
+      x_player                   <= x_p2;
+      y_player                   <= y_p2;
+      up_player                  <= up_p2;
+      left_player                <= left_p2;
+      right_player               <= right_p2;
+      down_player                <= down_p2;
+      IF (hitbox_count_players > "000001110100") THEN -- should be the above but doubled, for the reset
+       start_hitbox_count_players <= '0';--the reset
+       new_switch_state           <= P1;
+       x_p2                       <= new_x_player; -- output the new location for P2
+       y_p2                       <= new_y_player;-- or new_?
+      ELSE
+       new_switch_state <= P2;
+      END IF;
+    END CASE;
+   END PROCESS;
+   --------------
+   PROCESS (right_player, left_player, up_player, down_player, dir_state, y_player, x_player, hitbox_count_players, move_player)
+    BEGIN
+     CASE dir_state IS
+      WHEN begin_state =>
+       new_state      <= which_direction;
+       new_x_player   <= x_player;
+       new_y_player   <= y_player;
+       check_x_player <= "0000";--- could these be removed? the output of this isnt important at this state
+       check_y_player <= "0000";
+      WHEN which_direction =>
+       check_x_player <= "0000";
+       check_y_player <= "0000";
+       IF (hitbox_count_players = "000000110100" OR (hitbox_count_players > "000001110100")) THEN--- so dont go to attempt state if new player is inserted in fsm
+        new_state <= which_direction;
+       ELSE
+        IF ((down_player = '0') AND (up_player = '0') AND (left_player = '0') AND (right_player = '1')) THEN ---maybe change this back to priority case
+         new_state <= attempt_to_right;
+        ELSIF ((down_player = '0') AND (up_player = '0') AND (left_player = '1') AND (right_player = '0')) THEN
+         new_state <= attempt_to_left;
+        ELSIF ((down_player = '0') AND (up_player = '1') AND (left_player = '0') AND (right_player = '0')) THEN
+         new_state <= attempt_to_up;
+        ELSIF ((down_player = '1') AND (up_player = '0') AND (left_player = '0') AND (right_player = '0')) THEN
+         new_state <= attempt_to_down;
+        ELSE
+         check_x_player <= "0000";
+         check_y_player <= "0000";
+         new_state      <= which_direction;
+        END IF;
+       END IF;
+       --attempt states
+      WHEN attempt_to_right =>
+       new_x_player   <= x_player;
+       new_y_player   <= y_player;
+       check_x_player <= std_logic_vector(unsigned(x_player) + "0001");
+       check_y_player <= (y_player);
+       IF (move_player = '1') THEN
+        new_state <= right_output;
+       ELSE
+        new_state <= which_direction;
+       END IF;
+      WHEN attempt_to_left =>
+       new_x_player   <= x_player;
+       new_y_player   <= y_player;
+       check_x_player <= std_logic_vector(unsigned(x_player) - 1);
+       check_y_player <= (y_player);
+       IF (move_player = '1') THEN
+        new_state <= left_output;
+       ELSE
+        new_state <= which_direction;
+       END IF;
+      WHEN attempt_to_up =>
+       new_x_player   <= x_player;
+       new_y_player   <= y_player;
+       check_x_player <= (x_player);
+       check_y_player <= std_logic_vector(unsigned(y_player) - 1);
+       IF (move_player = '1') THEN
+        new_state <= up_output;
+       ELSE
+        new_state <= which_direction;
+       END IF;
+      WHEN attempt_to_down =>
+       new_x_player   <= x_player;
+       new_y_player   <= y_player;
+       check_x_player <= (x_player);
+       check_y_player <= std_logic_vector(unsigned(y_player) + 1);
+       IF (move_player = '1') THEN
+        new_state <= down_output;
+       ELSE
+        new_state <= which_direction;
+       END IF;
+       --- output states
+      WHEN right_output =>
+       new_x_player   <= std_logic_vector(unsigned(x_player) + "0001");
+       new_y_player   <= y_player;
+       check_x_player <= "0000";
+       check_y_player <= "0000";
+       IF (switch_players = '1') THEN --
+        new_state <= which_direction;
+       ELSE
+        new_state <= right_output;
+       END IF;
+      WHEN left_output =>
+       new_x_player   <= std_logic_vector(unsigned(x_player) - "0001");
+       new_y_player   <= y_player;
+       check_x_player <= "0000";
+       check_y_player <= "0000";
+       IF (switch_players = '1') THEN --
+        new_state <= which_direction;
+       ELSE
+        new_state <= left_output;
+       END IF;
+      WHEN up_output =>
+       new_x_player   <= x_player;
+       new_y_player   <= std_logic_vector(unsigned(y_player) - "0001");
+       check_x_player <= "0000";
+       check_y_player <= "0000";
+       IF (switch_players = '1') THEN --
+        new_state <= which_direction;
+       ELSE
+        new_state <= up_output;
+       END IF;
+      WHEN down_output =>
+       new_x_player   <= x_player;
+       new_y_player   <= std_logic_vector(unsigned(y_player) + "0001");
+       check_x_player <= "0000";
+       check_y_player <= "0000";
+       IF (switch_players = '1') THEN -- this signal changes when P2 goes to one or reversed
+        new_state <= which_direction;
+       ELSE
+        new_state <= down_output;
+       END IF;
+     END CASE;
+    END PROCESS;
+    ------------- Check if there's an obstacle module for P1 (might be a problem that this doesnt update on clock)
+    PROCESS (clk, walls_and_crates, check_x_player, check_y_player, bomb_x_a, bomb_y_a, bomb_a_active, bomb_x_b, bomb_y_b, bomb_b_active)---bomb_x_c, bomb_y_c, bomb_c_active,bomb_x_d, bomb_y_d, bomb_d_active,bomb_x_e, bomb_y_e, bomb_e_active,bomb_x_f, bomb_y_f, bomb_f_active,bomb_x_g, bomb_y_g, bomb_g_active,bomb_x_h, bomb_y_h, bomb_h_active
+     BEGIN
+      IF (
+       (walls_and_crates(to_integer(unsigned(check_x_player)) + to_integer(unsigned(check_y_player)) * 11) = '0')
+       AND (bomb_x_a /= std_logic_vector(check_x_player) OR (bomb_y_a /= std_logic_vector(check_y_player)) OR (bomb_a_active = '0'))
+       AND (bomb_x_b /= std_logic_vector(check_x_player) OR (bomb_y_b /= std_logic_vector(check_y_player)) OR(bomb_b_active = '0'))
+       AND (bomb_x_c /= std_logic_vector(check_x_player) OR (bomb_y_c /= std_logic_vector(check_y_player)) OR(bomb_c_active = '0'))
+       AND (bomb_x_d /= std_logic_vector(check_x_player) OR (bomb_y_d /= std_logic_vector(check_y_player)) OR(bomb_d_active = '0'))
+       AND (bomb_x_e /= std_logic_vector(check_x_player) OR (bomb_y_e /= std_logic_vector(check_y_player)) OR(bomb_e_active = '0'))
+       AND (bomb_x_f /= std_logic_vector(check_x_player) OR (bomb_y_f /= std_logic_vector(check_y_player)) OR(bomb_f_active = '0'))
+       AND (bomb_x_g /= std_logic_vector(check_x_player) OR (bomb_y_g /= std_logic_vector(check_y_player)) OR(bomb_g_active = '0'))
+       AND (bomb_x_h /= std_logic_vector(check_x_player) OR (bomb_y_h /= std_logic_vector(check_y_player)) OR(bomb_h_active = '0'))
+       ) THEN
+       move_player <= '1';
+      ELSE
+       move_player <= '0';
+      END IF;
+      END PROCESS;
+END hitbox_behaviour;
--- vhdl/hitbox/hitbox.vhd
+++ vhdl/hitbox/hitbox.vhd
@@ -1,30 +1,45 @@
-library IEEE;
-use IEEE.std_logic_1164.ALL;
-
-entity hitbox is
-   port(clk		 : in  std_logic;
-	reset	 : in  std_logic;
-        walls_and_crates   : in  std_logic_vector(120 downto 0);
-	bomb_x_a	 : in  std_logic_vector(3 downto 0);
-	bomb_y_a	 : in  std_logic_vector(3 downto 0);
-	bomb_x_b	 : in  std_logic_vector(3 downto 0);
-	bomb_y_b	 : in  std_logic_vector(3 downto 0);
-
-	bomb_a_active : in std_logic;
-	bomb_b_active : in std_logic;
-
-        up_p1    : in  std_logic;
-        right_p1 : in  std_logic;
-        down_p1  : in  std_logic;
-        left_p1  : in  std_logic;
-        up_p2    : in  std_logic;
-        right_p2 : in  std_logic;
-        down_p2  : in  std_logic;
-        left_p2  : in  std_logic;
-        x_p1 : out std_logic_vector(3 downto 0);
-        y_p1 : out std_logic_vector(3 downto 0);
-        x_p2 : out std_logic_vector(3 downto 0);
-        y_p2 : out std_logic_vector(3 downto 0));
-
-end hitbox;
-
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+ENTITY hitbox IS
+ PORT (
+  clk              : IN std_logic;
+  reset            : IN std_logic;
+  walls_and_crates : IN std_logic_vector(120 DOWNTO 0);
+  bomb_x_a         : IN std_logic_vector(3 DOWNTO 0);
+  bomb_y_a         : IN std_logic_vector(3 DOWNTO 0);
+  bomb_x_b         : IN std_logic_vector(3 DOWNTO 0);
+  bomb_y_b         : IN std_logic_vector(3 DOWNTO 0);
+  bomb_x_c         : IN std_logic_vector(3 DOWNTO 0);
+  bomb_y_c         : IN std_logic_vector(3 DOWNTO 0);
+  bomb_x_d         : IN std_logic_vector(3 DOWNTO 0);
+  bomb_y_d         : IN std_logic_vector(3 DOWNTO 0);
+  bomb_x_e         : IN std_logic_vector(3 DOWNTO 0);
+  bomb_y_e         : IN std_logic_vector(3 DOWNTO 0);
+  bomb_x_f         : IN std_logic_vector(3 DOWNTO 0);
+  bomb_y_f         : IN std_logic_vector(3 DOWNTO 0);
+  bomb_x_g         : IN std_logic_vector(3 DOWNTO 0);
+  bomb_y_g         : IN std_logic_vector(3 DOWNTO 0);
+  bomb_x_h         : IN std_logic_vector(3 DOWNTO 0);
+  bomb_y_h         : IN std_logic_vector(3 DOWNTO 0);
+  bomb_a_active    : IN std_logic;
+  bomb_b_active    : IN std_logic;
+  bomb_c_active    : IN std_logic;
+  bomb_d_active    : IN std_logic;
+  bomb_e_active    : IN std_logic;
+  bomb_f_active    : IN std_logic;
+  bomb_g_active    : IN std_logic;
+  bomb_h_active    : IN std_logic;
+  up_p1            : IN std_logic;
+  right_p1         : IN std_logic;
+  down_p1          : IN std_logic;
+  left_p1          : IN std_logic;
+  up_p2            : IN std_logic;
+  right_p2         : IN std_logic;
+  down_p2          : IN std_logic;
+  left_p2          : IN std_logic;
+  x_p1             : OUT std_logic_vector(3 DOWNTO 0);
+  y_p1             : OUT std_logic_vector(3 DOWNTO 0);
+  x_p2             : OUT std_logic_vector(3 DOWNTO 0);
+  y_p2             : OUT std_logic_vector(3 DOWNTO 0)
+ );
+END hitbox;
