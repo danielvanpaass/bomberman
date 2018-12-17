3d3561dd3aeafe19358b494f902b79237bc20f15
MichaelMiao99 <45036567+MichaelMiao99@users.noreply.github.com>
2018-12-17 22:36:06 +0100

    Add files via upload

---
+++ vga/sprites/sprites_tb.vhd
@@ -0,0 +1,6 @@
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+
+entity sprites_tb is
+end sprites_tb;
+
---
+++ vga/sprites/sprites_tb_behaviour_cfg.vhd
@@ -0,0 +1,6 @@
+configuration sprites_tb_behaviour_cfg of sprites_tb is
+   for behaviour
+      for all: sprites use configuration work.sprites_behaviour_cfg;
+      end for;
+   end for;
+end sprites_tb_behaviour_cfg;
