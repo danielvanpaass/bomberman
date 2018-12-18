662f59a1d72064b01853d64045547fa2340657c9
danielvanpaass <38658984+danielvanpaass@users.noreply.github.com>
2018-12-18 23:31:54 +0100

    Update sprites-behaviour.vhd

--- vga/sprites/sprites-behaviour.vhd
+++ vga/sprites/sprites-behaviour.vhd
@@ -51,7 +51,7 @@ elsif   (x_map = x_bomb_a and y_map = y_bomb_a and bomb_a_enable = '1' ) OR --bo
 	else
 	rgb <= "111";
 	end if;	
-elsif    coordinates_vector((10-(to_integer(unsigned(y_map)))*11 + (10-to_integer(unsigned(x_map))) * 2 + 1) downto (to_integer(unsigned(y_map))*11 + to_integer(unsigned(x_map)) * 2) ) = "10" then--crate 
+elsif    coordinates_vector(((10-(to_integer(unsigned(y_map)))*11 + (10-to_integer(unsigned(x_map))) * 2 + 1) = "1") AND  ((to_integer(unsigned(y_map))*11 + to_integer(unsigned(x_map)) * 2) ) = "0") then--crate 
 	if (crate_vector(to_integer(unsigned(h_map)+to_integer(unsigned(v_map))*11)) = '1') then
 	rgb <= "110";
 	else
@@ -59,8 +59,6 @@ elsif    coordinates_vector((10-(to_integer(unsigned(y_map)))*11 + (10-to_intege
 	end if;	
 elsif    coordinates_vector((10-to_integer(unsigned(y_map))*11 + (10-to_integer(unsigned(x_map))) * 2 + 1) downto (to_integer(unsigned(y_map))*11 + to_integer(unsigned(x_map)) * 2) ) = "01" then--empty 
 	rgb <= "111";
---elsif    coordinates_vector((10-to_integer(unsigned(y_map))*11 + to_integer(unsigned(x_map)) * 2 + 1) downto (to_integer(unsigned(y_map))*11 + to_integer(unsigned(x_map)) * 2) ) = "01" then
---	rgb<= "111";-- in case of simple empty tiles
 elsif    coordinates_vector((to_integer(unsigned(y_map))*11 + (10-to_integer(unsigned(x_map)) )* 2 + 1) downto (to_integer(unsigned(y_map))*11 + to_integer(unsigned(x_map)) * 2) ) = "00" then--explosion 
 	if (explosion_vector(to_integer(unsigned(h_map)+to_integer(unsigned(v_map))*11)) = '1') then
 	rgb <= "000";
