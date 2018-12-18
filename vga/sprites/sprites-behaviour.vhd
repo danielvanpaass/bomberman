b285a7970e3beb6f18bc058f771ef3bd40ee6664
danielvanpaass <38658984+danielvanpaass@users.noreply.github.com>
2018-12-18 23:52:08 +0100

    Update sprites-behaviour.vhd

--- vga/sprites/sprites-behaviour.vhd
+++ vga/sprites/sprites-behaviour.vhd
@@ -6,7 +6,7 @@ architecture behaviour of sprites is
  signal v_map : std_logic_vector (3 downto 0);
 signal h_map : std_logic_vector (3 downto 0);
 signal coordinates_vector : std_logic_vector (241 downto 0);
-signal P_vector, bomb_vector, crate_vector, explosion_vector, wall_vector : std_logic_vector (120 downto 0);
+signal P_vector, bomb_vector, crate_vector, explosion_vector, wall_vector : std_logic_vector (0 to 120);
 begin
 v_map <= input_v_map(5 downto 2);
 h_map <= input_h_map(4 downto 1);
@@ -51,15 +51,15 @@ elsif   (x_map = x_bomb_a and y_map = y_bomb_a and bomb_a_enable = '1' ) OR --bo
 	else
 	rgb <= "111";
 	end if;	
-elsif    coordinates_vector(((10-(to_integer(unsigned(y_map)))*11 + (10-to_integer(unsigned(x_map))) * 2 + 1) = '1') AND  ((to_integer(unsigned(y_map))*11 + to_integer(unsigned(x_map)) * 2) ) = '0') then--crate 
-	if (crate_vector(to_integer(unsigned(h_map)+to_integer(unsigned(v_map))*11)) = '1') then
+elsif   (coordinates_vector( ( 10-to_integer(unsigned(y_map)) ) *22  + (10-to_integer(unsigned(x_map)))*2 ) = '1' )AND (coordinates_vector( ( 10-to_integer(unsigned(y_map)) ) *22+ (10-to_integer(unsigned(x_map)))*2+1) = '0') then
+	if (crate_vector(to_integer(unsigned(h_map)+to_integer(unsigned(v_map))*11)) = '1') then--crate
 	rgb <= "110";
 	else
 	rgb <= "111";
 	end if;	
-elsif    coordinates_vector((10-to_integer(unsigned(y_map))*11 + (10-to_integer(unsigned(x_map))) * 2 + 1) downto (to_integer(unsigned(y_map))*11 + to_integer(unsigned(x_map)) * 2) ) = "01" then--empty 
+elsif   (coordinates_vector( ( 10-to_integer(unsigned(y_map)) ) *22 + (10-to_integer(unsigned(x_map)))*2)='0')AND (coordinates_vector( ( 10-to_integer(unsigned(y_map)) ) *22+  (10-to_integer(unsigned(x_map)))*2+1) = '1') then--empty 
 	rgb <= "111";
-elsif    coordinates_vector((to_integer(unsigned(y_map))*11 + (10-to_integer(unsigned(x_map)) )* 2 + 1) downto (to_integer(unsigned(y_map))*11 + to_integer(unsigned(x_map)) * 2) ) = "00" then--explosion 
+elsif   (coordinates_vector( ( 10-to_integer(unsigned(y_map)) ) *22+  (10-to_integer(unsigned(x_map)))*2) = '0')AND (coordinates_vector( ( 10-to_integer(unsigned(y_map)) ) *22+  (10-to_integer(unsigned(x_map)))*2+1) = '0') then--explosion 
 	if (explosion_vector(to_integer(unsigned(h_map)+to_integer(unsigned(v_map))*11)) = '1') then
 	rgb <= "000";
 	else
@@ -74,6 +74,3 @@ else ---must be 11 so wall
 end if;	
 end process;
 end behaviour;
-
-
-
