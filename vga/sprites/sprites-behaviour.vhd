94c00948d1282148dbedeb5e5e809bacc14dff8d
danielvanpaass <38658984+danielvanpaass@users.noreply.github.com>
2018-12-18 23:33:38 +0100

    Update sprites-behaviour.vhd

--- vga/sprites/sprites-behaviour.vhd
+++ vga/sprites/sprites-behaviour.vhd
@@ -51,7 +51,7 @@ elsif   (x_map = x_bomb_a and y_map = y_bomb_a and bomb_a_enable = '1' ) OR --bo
 	else
 	rgb <= "111";
 	end if;	
-elsif    coordinates_vector(((10-(to_integer(unsigned(y_map)))*11 + (10-to_integer(unsigned(x_map))) * 2 + 1) = "1") AND  ((to_integer(unsigned(y_map))*11 + to_integer(unsigned(x_map)) * 2) ) = "0") then--crate 
+elsif    coordinates_vector(((10-(to_integer(unsigned(y_map)))*11 + (10-to_integer(unsigned(x_map))) * 2 + 1) = '1') AND  ((to_integer(unsigned(y_map))*11 + to_integer(unsigned(x_map)) * 2) ) = '0') then--crate 
 	if (crate_vector(to_integer(unsigned(h_map)+to_integer(unsigned(v_map))*11)) = '1') then
 	rgb <= "110";
 	else
