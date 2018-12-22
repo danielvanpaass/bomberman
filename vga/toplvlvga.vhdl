d74cbc8d5a26d4f93d2325951b6c5a58c5c3dfea
danielvanpaass <38658984+danielvanpaass@users.noreply.github.com>
2018-12-22 12:43:46 +0100

    top lvl video_on

--- vga/toplvlvga.vhdl
+++ vga/toplvlvga.vhdl
@@ -71,7 +71,8 @@ PORT
 		x_out      : OUT std_logic_vector(3 DOWNTO 0);
 		y_out      : OUT std_logic_vector(3 DOWNTO 0);
 		h_out      : OUT std_logic_vector(4 DOWNTO 0);
-		v_out      : OUT std_logic_vector(5 DOWNTO 0)
+		v_out      : OUT std_logic_vector(5 DOWNTO 0);
+		video_on   : OUT std_logic
 	);
 END component;
 component sprites 
@@ -111,15 +112,18 @@ playground: in  std_logic_vector(241 downto 0);
         y_map         : in  std_logic_vector(3 downto 0);
         input_h_map         : in  std_logic_vector(4 downto 0);
         input_v_map         : in  std_logic_vector(5 downto 0);
+	video_on: in std_logic;
 	red : out std_logic;
 	green : out std_logic;
-	blue: out std_logic);      
+	blue: out std_logic
+	);      
 end component;
 signal x_out_temp, y_out_temp  :  std_logic_vector(3 DOWNTO 0);
 signal h_out_temp      : std_logic_vector(4 DOWNTO 0);
 signal v_out_temp     : std_logic_vector(5 DOWNTO 0);
+signal video : std_logic;
 
 begin
-map1: vga_controller port map (clk, reset, vga_hsync, vga_vsync, clock_60hz, x_out_temp, y_out_temp, h_out_temp, v_out_temp);
-map2: sprites port map ( victory, playground, clk , reset , x_p1, y_p1, x_p2, y_p2, x_bomb_a, y_bomb_a, bomb_a_enable, x_bomb_b, y_bomb_b, bomb_b_enable, x_bomb_c, y_bomb_c, bomb_c_enable, x_bomb_d, y_bomb_d, bomb_d_enable, x_bomb_e, y_bomb_e, bomb_e_enable, x_bomb_f, y_bomb_f, bomb_f_enable, x_bomb_g, y_bomb_g, bomb_g_enable, x_bomb_h, y_bomb_h, bomb_h_enable, x_out_temp, y_out_temp, h_out_temp, v_out_temp, red, green, blue  );
+map1: vga_controller port map (clk, reset, vga_hsync, vga_vsync, clock_60hz, x_out_temp, y_out_temp, h_out_temp, v_out_temp, video);
+map2: sprites port map ( victory, playground, clk , reset , x_p1, y_p1, x_p2, y_p2, x_bomb_a, y_bomb_a, bomb_a_enable, x_bomb_b, y_bomb_b, bomb_b_enable, x_bomb_c, y_bomb_c, bomb_c_enable, x_bomb_d, y_bomb_d, bomb_d_enable, x_bomb_e, y_bomb_e, bomb_e_enable, x_bomb_f, y_bomb_f, bomb_f_enable, x_bomb_g, y_bomb_g, bomb_g_enable, x_bomb_h, y_bomb_h, bomb_h_enable, x_out_temp, y_out_temp, h_out_temp, v_out_temp,video, red, green, blue  );
 end behaviour;
