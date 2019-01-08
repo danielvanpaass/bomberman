1145712fb6f7e3e36adf041d1cafd2c3c39e20c0
MichaelMiao99 <45036567+MichaelMiao99@users.noreply.github.com>
2019-01-08 08:42:22 +0100

    Add files via upload

--- Hitbox/VHDL/hitbox-behaviour.vhd
+++ Hitbox/VHDL/hitbox-behaviour.vhd
@@ -3,16 +3,18 @@ USE IEEE.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 ARCHITECTURE hitbox_behaviour OF hitbox IS
  TYPE state_type IS (begin_state, which_direction, attempt_to_right, attempt_to_left, attempt_to_up, attempt_to_down, right_output, left_output, up_output, down_output, stay_output);
- SIGNAL dir_p1_state, new_state_p1 : state_type;
-SIGNAL dir_p2_state, new_state_p2 : state_type;
- SIGNAL check_x_p1, check_y_p1 : std_logic_vector (3 DOWNTO 0);
-SIGNAL check_x_p2, check_y_p2 : std_logic_vector (3 DOWNTO 0);
- SIGNAL hitbox_count_players : std_logic_vector (2 DOWNTO 0);
- SIGNAL begin_counting : std_logic; ---!!!
- SIGNAL count_players, new_count_players : unsigned (2 DOWNTO 0);
+ TYPE switch_type IS (begin_state, P1, P2, fromP1toP2, fromP2toP1);
+ SIGNAL dir_state, new_state : state_type;
+ SIGNAL switch_state, new_switch_state : switch_type;
+ SIGNAL new_x_player, new_y_player, x_player, y_player : STD_logic_vector (3 DOWNTO 0);
+ SIGNAL check_x_player, check_y_player : std_logic_vector (3 DOWNTO 0);
+ SIGNAL hitbox_count_players : std_logic_vector (3 DOWNTO 0);
+ SIGNAL begin_counting, move_player, up_player, down_player, right_player, left_player, switch_players : std_logic;
+ SIGNAL count_players, new_count_players : unsigned (3 DOWNTO 0);
 SIGNAL walls_and_crates_inverted : std_logic_vector(0 to 120);
-
- CONSTANT cooldown_end : std_logic_vector := "111"; ---"0101111101011110000100"--this equalshalf switch_to_p1,
+SIGNAL x_p1_local, y_p1_local, x_p2_local, y_p2_local : std_logic_vector( 3 downto 0);
+ CONSTANT switch_to_p2 : std_logic_vector := "0011"; ---"0101111101011110000100"--this equalshalf switch_to_p1,
+ CONSTANT switch_to_p1 : std_logic_vector := "1000"; --:= "1011111010111100001000"- this equals0.25 seconds, meaning that a full cycle of P1+P2 turn is in 0.25 seconds.
 --however, for simulation purposes we used much smaller values.
 BEGIN
 walls_and_crates_inverted <= walls_and_crates;
@@ -24,11 +26,11 @@ y_p2<=y_p2_local;
  BEGIN
   IF rising_edge (v_clk) THEN
    IF reset = '1' THEN -- reset the whole system
-    dir_p1_state <= begin_state;
-    dir_p2_state <= begin_state;
+    dir_state <= begin_state;
+    switch_state <= begin_state;
    ELSE
-    dir_p1_state <= new_state_p1;
-    dir_p2_state <= new_state_p2;
+    dir_state <= new_state;
+    switch_state <= new_switch_state;
    END IF;
   END IF;
  END PROCESS;
@@ -44,60 +46,139 @@ BEGIN
 	END IF;
   END IF;
  END PROCESS;
-
  -- counter for P1 and P2 playtime
  PROCESS (count_players)
  BEGIN
   new_count_players <= count_players + 1;
  END PROCESS;
  hitbox_count_players <= std_logic_vector (count_players);
-
- PROCESS (right_p1, left_p1, up_p1, down_p1, dir_p1_state, hitbox_count_players, x_p1_local, y_p1_local)
+ --------- decides which player is to play
+ PROCESS (switch_state, hitbox_count_players)
  BEGIN
-  CASE dir_p1_state IS
+  CASE switch_state IS
    WHEN begin_state =>
+    switch_players <= '0';
+    x_player <= "0001";
+    y_player <= "0001";
     x_p1_local <= "0001";
     y_p1_local <= "0001";
+    x_p2_local <= "1001";
+    y_p2_local <= "1001";
+    new_switch_state <= P1;
+    begin_counting <= '0';
+    up_player <= '0';
+    left_player <= '0';
+    right_player <= '0';
+    down_player <= '0';
+   WHEN P1 =>
+    x_player <= x_p1_local;
+    y_player <= y_p1_local;
+    begin_counting <= '1';
+    up_player <= up_p1;
+    left_player <= left_p1;
+    right_player <= right_p1;
+    down_player <= down_p1;
+    IF ((hitbox_count_players = switch_to_p2)) THEN --P1 ends his turn
+     new_switch_state <= fromP1toP2;
+    ELSE
+     new_switch_state <= P1;
+     switch_players <= '0';
+    END IF;
+   WHEN fromP1toP2 =>
+    x_p1_local <= new_x_player;
+    y_p1_local <= new_y_player;
+    switch_players <= '1';
+    new_switch_state <= P2;
+    x_player <= new_x_player;--just to not have outputs undefined, the value of x_player shouldnt matter
+    y_player <= new_y_player;
+   WHEN P2 =>
+    begin_counting <= '1';
+    x_player <= x_p2_local;
+    y_player <= y_p2_local;
+    up_player <= up_p2;
+    left_player <= left_p2;
+    right_player <= right_p2;
+    down_player <= down_p2;
+    IF (hitbox_count_players = switch_to_p1) THEN -- should be the above but doubled, for the reset
+     new_switch_state <= fromP2toP1;
+
+    ELSE
+     new_switch_state <= P2;
+     switch_players <= '0';
+    END IF;
+   WHEN fromP2toP1 =>
+    new_switch_state <= P1;
+    begin_counting <= '0';
+    x_p2_local <= new_x_player; -- output the new location for P2
+    y_p2_local <= new_y_player;-- or new_?
+    switch_players <= '1';
+    x_player <= new_x_player;--just to not have outputs undefined, the value of x_player shouldnt matter
+    y_player <= new_y_player;
+  END CASE;
+ END PROCESS;
+ --------------
+ PROCESS (right_player, left_player, up_player, down_player, dir_state, hitbox_count_players, move_player, switch_players)
+ BEGIN
+  CASE dir_state IS
+   WHEN begin_state =>
     new_state <= which_direction;
-    check_x_p1 <= "0000";--- could these be removed? the output of this isnt important at this state
-    check_y_p1 <= "0000";--- could these be removed? the output of this isnt important at this state
+    new_x_player <= x_player;--- could these be removed? the output of this isnt important at this state
+    new_y_player <= y_player;--- could these be removed? the output of this isnt important at this state
+    check_x_player <= "0000";--- could these be removed? the output of this isnt important at this state
+    check_y_player <= "0000";--- could these be removed? the output of this isnt important at this state
 
    WHEN which_direction =>
-    check_x_p1 <= "0000";
-    check_y_p1 <= "0000";
-    IF ((down_p1 = '0') AND (up_p1 = '0') AND (left_p1 = '0') AND (right_p1 = '1')) THEN ---maybe change this back to priority case
-      new_state <= attempt_to_right;
-    ELSIF ((down_p1 = '0') AND (up_p1 = '0') AND (left_p1 = '1') AND (right_p1 = '0')) THEN
-      new_state <= attempt_to_left;
-    ELSIF ((down_p1 = '0') AND (up_p1 = '1') AND (left_p1 = '0') AND (right_p1 = '0')) THEN
-      new_state <= attempt_to_up;
-    ELSIF ((down_p1 = '1') AND (up_p1 = '0') AND (left_p1 = '0') AND (right_p1 = '0')) THEN
-      new_state <= attempt_to_down;
+    check_x_player <= "0000";
+    check_y_player <= "0000";
+    IF (switch_players = '1') THEN--- so dont go to attempt state if new player is inserted in fsm
+     new_state <= which_direction;
     ELSE
-      new_state <= which_direction;
+     IF ((down_player = '0') AND (up_player = '0') AND (left_player = '0') AND (right_player = '1')) THEN ---maybe change this back to priority case
+      new_state <= attempt_to_right;
+     ELSIF ((down_player = '0') AND (up_player = '0') AND (left_player = '1') AND (right_player = '0')) THEN
+      new_state <= attempt_to_left;
+     ELSIF ((down_player = '0') AND (up_player = '1') AND (left_player = '0') AND (right_player = '0')) THEN
+      new_state <= attempt_to_up;
+     ELSIF ((down_player = '1') AND (up_player = '0') AND (left_player = '0') AND (right_player = '0')) THEN
+      new_state <= attempt_to_down;
+     ELSE
+      new_state <= stay_output;
+      new_x_player <= x_player;
+      new_y_player <= y_player;
+     END IF;
     END IF;
     --attempt states
 
    WHEN attempt_to_right =>
-    new_x_p1 <= x_p1;
-    new_y_p1 <= y_p1;
-    check_x_p1 <= std_logic_vector(unsigned(x_p1) + "0001");
-    check_y_p1 <= (y_p1);
+    new_x_player <= x_player;
+    new_y_player <= y_player;
+    check_x_player <= std_logic_vector(unsigned(x_player) + "0001");
+    check_y_player <= (y_player);
+    IF (move_player = '1') THEN
      new_state <= right_output;
+    ELSE
+     new_state <= stay_output;
+    END IF;
    WHEN attempt_to_left =>
-    new_x_p1 <= x_p1;
-    new_y_p1 <= y_p1;
-    check_x_p1 <= std_logic_vector(unsigned(x_p1) - 1);
-    check_y_p1 <= (y_p1);
+    new_x_player <= x_player;
+    new_y_player <= y_player;
+    check_x_player <= std_logic_vector(unsigned(x_player) - 1);
+    check_y_player <= (y_player);
+    IF (move_player = '1') THEN
      new_state <= left_output;
-
+    ELSE
+     new_state <= stay_output;
+    END IF;
    WHEN attempt_to_up =>
-    new_x_p1 <= x_p1;
-    new_y_p1 <= y_p1;
-    check_x_p1 <= (x_p1);
-    check_y_p1 <= std_logic_vector(unsigned(y_p1) - 1);
+    new_x_player <= x_player;
+    new_y_player <= y_player;
+    check_x_player <= (x_player);
+    check_y_player <= std_logic_vector(unsigned(y_player) - 1);
+    IF (move_player = '1') THEN
      new_state <= up_output;
-
+    ELSE
+     new_state <= stay_output;
+    END IF;
    WHEN attempt_to_down =>
     new_x_player <= x_player;
     new_y_player <= y_player;
