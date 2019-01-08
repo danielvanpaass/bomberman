13e8f6838cac6a5bd45fb11a26868f2ba0529961
Roffid <43887079+Roffid@users.noreply.github.com>
2019-01-08 09:05:39 +0100

    Update bombcook-behaviour.vhd

--- FinalCode/bombcook-behaviour.vhd
+++ FinalCode/bombcook-behaviour.vhd
@@ -6,7 +6,7 @@ architecture behaviour of bombcook is
 type bombcook_states is (nobomb, cook, still_cook, explosion_flag, explode);
 
 signal state, new_state: bombcook_states;
-signal bombxsample, bombysample : std_logic_vector(3 downto 0);
+signal bombxsample, bombysample, new_bombxsample, new_bombysample : std_logic_vector(3 downto 0);
 
 begin
 
@@ -17,6 +17,8 @@ process (clk, reset)
        state <= nobomb;
      else
        state <= new_state;
+		 bombxsample <= new_bombxsample;
+		 bombysample <= new_bombysample;
      end if;
    end if;
 end process;
@@ -28,6 +30,8 @@ process (state, bombinit, expl,bombxsample,bombysample,playerx,playery)
 		when nobomb =>
 		bombx <= "0000";
 		bomby <= "0000";
+		new_bombxsample <= "0000";
+		new_bombysample <= "0000";
 		bombdisplay <= '0';
 		expl_flag <= '0';
 		if (bombinit = '1') then
@@ -37,8 +41,8 @@ process (state, bombinit, expl,bombxsample,bombysample,playerx,playery)
 		end if;
 
 		when cook =>
-		bombxsample <= playerx;
-		bombysample <= playery;
+		new_bombxsample <= playerx;
+		new_bombysample <= playery;
 		bombx <= "0000";
 		bomby <= "0000";
 		bombdisplay <= '0';
@@ -46,6 +50,8 @@ process (state, bombinit, expl,bombxsample,bombysample,playerx,playery)
 		new_state <= still_cook;
 
 		when still_cook =>
+		new_bombxsample <= bombxsample;
+		new_bombysample <= bombysample;
 		bombx <= bombxsample;
 		bomby <= bombysample;
 		bombdisplay <= '1';
@@ -57,6 +63,8 @@ process (state, bombinit, expl,bombxsample,bombysample,playerx,playery)
 		end if;
 
 		when explosion_flag =>
+		new_bombxsample <= bombxsample;
+		new_bombysample <= bombysample;
 		bombx <= bombxsample;
 		bomby <= bombysample;
 		bombdisplay <= '1';
@@ -64,6 +72,8 @@ process (state, bombinit, expl,bombxsample,bombysample,playerx,playery)
 		new_state <= explode;
 
 		when explode =>
+		new_bombxsample <= bombxsample;
+		new_bombysample <= bombysample;
 		bombx <= bombxsample;
 		bomby <= bombysample;
 		bombdisplay <= '1';
@@ -78,4 +88,3 @@ process (state, bombinit, expl,bombxsample,bombysample,playerx,playery)
 end process;
 
 end behaviour;
-
