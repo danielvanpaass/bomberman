5dac33d4d7882b55495c60d0c7d7e936f08cc0ae
jorisvanbreukelen <38658948+jorisvanbreukelen@users.noreply.github.com>
2019-01-08 11:27:28 +0100

    Update VGA_control.vhd

--- vga/vga_new/VGA_control.vhd
+++ vga/vga_new/VGA_control.vhd
@@ -38,7 +38,7 @@ ARCHITECTURE behaviour OF VGA_controller IS
 	SIGNAL v_count ,   new_v_count                     : std_logic_vector(9 DOWNTO 0);
 	SIGNAL h_sync, v_sync, output_clock : std_logic;
 	CONSTANT begin_video : std_logic_vector(8 downto 0):= "000000000";
-	CONSTANT end_video  : std_logic_vector(8 downto 0):= "101000000";--320
+	CONSTANT end_video  : std_logic_vector(8 downto 0):= "011110010";--242
 BEGIN
 PROCESS (h_count, v_count)
 begin
