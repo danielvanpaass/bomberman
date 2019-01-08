a78b1ad7617aab288a515385842fdbd30009da3a
danielvanpaass <38658984+danielvanpaass@users.noreply.github.com>
2019-01-08 20:39:09 +0100

    Update VGA_control.vhd

--- vga/vga_new/VGA_control.vhd
+++ vga/vga_new/VGA_control.vhd
@@ -21,7 +21,7 @@ END VGA_controller;
 ARCHITECTURE behaviour OF VGA_controller IS
 	-- type states   
 	TYPE Position_states IS ( H_adder, H_reset, Reset_vga);
-	TYPE Block_states IS (H_reg, h_adder, x_adder, v_adder, y_adder, Reset_bl, wait_for_next_v_count);
+	TYPE Block_states IS (H_reg, h_adder, x_adder, v_adder, y_adder, begin_state, reset_position);
 	TYPE Hor_sync_states IS (H_High, H_Low);
 	TYPE Ver_sync_states IS (V_High, V_Low);
 
@@ -36,22 +36,29 @@ ARCHITECTURE behaviour OF VGA_controller IS
 	SIGNAL v,new_v                      : std_logic_vector(5 DOWNTO 0);
 	SIGNAL h_count, new_h_count                      : std_logic_vector(8 DOWNTO 0);
 	SIGNAL v_count ,   new_v_count                     : std_logic_vector(9 DOWNTO 0);
-	SIGNAL h_sync, v_sync, output_clock : std_logic;
+	SIGNAL h_sync, v_sync: std_logic;
 	CONSTANT begin_video : std_logic_vector(8 downto 0):= "000000000";
 	CONSTANT end_video  : std_logic_vector(8 downto 0):= "011110010";--242
 BEGIN
 PROCESS (h_count, v_count)
-begin
+begin-----to make sure that the rgb module does not paint outside the rectangle
 if h_count > begin_video AND h_count < end_video AND ( v_count < "0111011001" )  then --at this v_count value the last pixel of x10 y10 has been painted
 	video_on <= '1';
 else
 	video_on <= '0';
 end if;
---if x="0000" and h="00000" then
---	video_on <= '0';
---else
---	video_on <= '1';
---end if;
+end process;
+
+
+-----------------------------
+--to create a 60Hz clock--
+PROCESS ( y)
+begin-----to make sure that the rgb module does not paint outside the rectangle
+if (y="1010")  then --x y value the clock must be high, but it should not matter what  y value as long as it is periodic with 60Hz
+	clock_60hz <= '1';
+else
+	clock_60hz <= '0';
+end if;
 end process;
 
 	--------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -79,17 +86,14 @@ end process;
 			WHEN Reset_vga =>
 				new_v_count      <= "0000000000";
 				new_h_count      <= "000000000";
-				output_clock <= '1';
 				New_Position <= H_adder;
 
 			WHEN H_reset =>
 				new_h_count      <= "000000000";
 				new_v_count      <= std_logic_vector(unsigned(v_count) + to_unsigned(1,6));
 				New_Position <= H_adder;
-				output_clock <= '0';
 
 			WHEN H_adder =>
-				output_clock <= '0';
 				new_v_count <= v_count;
 				new_h_count      <= std_logic_vector(unsigned(h_count) + to_unsigned(1,6));
 				IF h_count = "110001111" AND v_count = "1000001100" THEN --399 524
@@ -100,27 +104,8 @@ end process;
 					New_Position <= H_adder;
 				END IF;
 
-			--WHEN V_hold => niet nodig, dit vertraagt alles met 1 clockcycle
---				output_clock <= '0';
---				new_h_count <= h_count;
---				new_v_count <= v_count;
----
---				IF v_count = "1000001100" AND (h_count="111011111") THEN 
---
---					New_Position <= Clock;
----				ELSE
---					New_Position <= H_reset;
---				END IF;
-
---			WHEN Clock => clock kan bij reset_vga gedaan worden
---				new_h_count <= h_count;
---				new_v_count <= v_count;
---				output_clock <= '1';
---				new_position <= Reset_vga;
 		END CASE;
 	END PROCESS;
-
-	clock_60hz <= output_clock;
 	------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	-- determining x y and h v
 	PROCESS (clk, reset, new_x, new_y, new_h, new_v)
@@ -132,7 +117,7 @@ end process;
 			y <= (others => '0');
 			h <= (others => '0'); 
 			v <= (others => '0');
-				Blocks <= Reset_bl;
+				Blocks <= begin_state;
 			ELSE
 			x <= new_x;
 			y <= new_y;
@@ -152,17 +137,17 @@ end process;
 	BEGIN
 		
 		CASE Blocks IS
-			WHen wait_for_next_v_count =>-- and reset values
-				If v_count = "0111011010" THEN
+			WHen reset_position =>-- and reset values
+				If v_count = "1000001100" and h_count = "110001111" THEN ---this is the maximum v_count and h_count value, so it has reached the end of screen
 					new_blocks <= H_reg;
 				ELSE
-					new_blocks <= wait_for_next_v_count;
+					new_blocks <= reset_position;
 				END IF;
 				new_h          <= "00000";
 				new_v          <= "000000";
 				new_x          <= "0000";
 				new_y          <= "0000";
-			WHEN Reset_bl =>
+			WHEN begin_state =>
 				new_blocks <= H_reg;
 				new_h          <= "00000";
 				new_v          <= "000000";
@@ -175,20 +160,10 @@ end process;
 			new_v <= v;
 				
 			
-				IF (h_count > begin_video) AND (h_count < end_video) AND  ( v_count < "0111011010" )  THEN --same values as video_on
+				IF (h_count > begin_video) AND (h_count < end_video)   THEN --same values as video_on
 					new_blocks <=  H_adder;
 				ELSE 
 					new_blocks <= H_reg;
-
-				--elsif h="10101" and x="1010" and v<"101011" then--21 and 10 and 43 ----if last pixel of block and not last row of block, add row
---
---					new_blocks <= v_adder;
---				elsif h = "10101" and (v="101011") and (x="1010")and (y<"1010") THEN --at x11 have to reset x and add y
---					new_blocks <= y_adder;
---				elsif h = "10101" and (v="101011") and (x="1010") and (y="1010") then
---					new_blocks <= reset_bl;
---				ELSE
---					new_blocks <= H_adder;
 				END IF;
 
 				
@@ -205,16 +180,11 @@ end process;
 			elsif h = "10100" and (v="101010") and (x="1010")and (y<"1010") THEN --at x11 have to reset x and add y
 				new_blocks <= y_adder;
 				elsif h = "10100" and (v="101010") and (x="1010") and (y="1010") then
-				new_blocks <= wait_for_next_v_count;
+				new_blocks <= reset_position;
 			ELSE
 				new_blocks <= H_adder;
 			end if;
-			--WHEN H_reset =>
-			--	new_x <= x;
-			--new_y <= y;
-			--new_v <= v;
-			--	new_h      <= "00000";
-			--	new_blocks <= x_adder;
+
 
 			WHEN v_adder =>
 			new_y <= y;
