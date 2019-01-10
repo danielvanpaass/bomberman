28594868e7ef8ad3aebb9d4f13d29337152a7c72
MichaelMiao99 <45036567+MichaelMiao99@users.noreply.github.com>
2019-01-10 18:35:47 +0100

    Update sprites-behaviour.vhd
    victory sprites, color adjusted

--- Bomberman 10-1-2019 einde dag/sprites-behaviour.vhd
+++ Bomberman 10-1-2019 einde dag/sprites-behaviour.vhd
@@ -5,7 +5,7 @@ USE IEEE.numeric_std.ALL;
 ARCHITECTURE behaviour OF sprites IS
 	SIGNAL v_map : std_logic_vector (3 DOWNTO 0);
 	SIGNAL h_map : std_logic_vector (3 DOWNTO 0);
-	SIGNAL P_vector, bomb_vector, crate_vector, explosion_vector, wall_vector , victory_vector_right, victory_vector_1_left, victory_vector_2_left: std_logic_vector (0 TO 120);
+	SIGNAL P_vector, bomb_vector, crate_vector, explosion_vector, wall_vector , victory_vector_1, victory_vector_2, victory_vector_3, victory_vector_4: std_logic_vector (0 TO 120);
 	SIGNAL check_1, check_2 : std_logic;
 	SIGNAL spritebit : INTEGER RANGE 0 TO 120;
 	SIGNAL spritebit_victory : INTEGER RANGE 0 TO 241;
@@ -13,9 +13,9 @@ ARCHITECTURE behaviour OF sprites IS
 BEGIN
 	v_map <= input_v_map(5 DOWNTO 2);
 	h_map <= input_h_map(4 DOWNTO 1);
-	victory_vector_1_left <= 			"1010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101";
-	victory_vector_2_left <= 			"0101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010";
- 	victory_vector_right <= 			"0101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010";
+	victory_vector_1 <= 			"1100011011111000110111110001100011101011000111010110001111111100011111111000111101110001111011100011100011011111000110111";
+	victory_vector_2 <= 			"1110110001111101100011100011100111000111101110001111011100011011111000110111110001100111100011000111110110001111101100011";
+ 	victory_vector_3 <= 			"0011111000001111111000011000110000110000000001111000000001111100000000111100000000011000011000110000111111100000111110000";
 	P_vector <= 			"0011111110000111111100011000001100110101011000101010100011111111101111111111111111111111111110111110001101100000111011100";
 	bomb_vector <= 			"0000001100000000100101000001000100001111000000111111000011110011000111110010001111111100011111111000011111100000011110000";
 	crate_vector <=			"1111111111111100000111111100011111011101110110011111001100011100011001111100110111011101111100011111110000011111111111111";
@@ -39,22 +39,51 @@ BEGIN
 					b <= "0000";
 				ELSE
 					IF victory(1) = '1' THEN	
-						IF(victory(0) = '1') THEN
-							IF (x_map = "0100"  AND y_map = "0101") THEN
-								IF (victory_vector_1_left(spritebit) = '1') THEN
+							IF (x_map = "0101"  AND y_map = "0101" AND victory(0) = '1') THEN
+								IF (P_vector(spritebit) = '1') THEN
+									r <= "1111";
+									g <= "0000";
+									b <= "0000";
+								ELSE 
 									r <= "1111";
 									g <= "1111";
+									b <= "1111";
+								END IF;
+							ELSIF (x_map = "0101"  AND y_map = "0101" AND victory(0) = '0') THEN
+								IF (P_vector(spritebit) = '1') THEN
+									r <= "0000";
+									g <= "0000";
 									b <= "1111";
 								ELSE 
 									r <= "1111";
 									g <= "1111";
 									b <= "1111";
 								END IF;
-							ELSIF (x_map = "0101" AND y_map = "0101") THEN
-								IF (victory_vector_right(spritebit) = '1') THEN
+							ELSIF (x_map = "0110" AND y_map = "0101") THEN
+								IF (victory_vector_1(spritebit) = '1') THEN
+									r <= "0000";
+									g <= "0000";
+									b <= "0000";
+								ELSE 
 									r <= "1111";
 									g <= "1111";
 									b <= "1111";
+								END IF;
+							ELSIF (x_map = "0111" AND y_map = "0101") THEN
+								IF (victory_vector_2(spritebit) = '1') THEN
+									r <= "0000";
+									g <= "0000";
+									b <= "0000";
+								ELSE 
+									r <= "1111";
+									g <= "1111";
+									b <= "1111";
+								END IF;
+							ELSIF (x_map = "1000" AND y_map = "0101") THEN
+								IF (victory_vector_1(spritebit) = '1') THEN
+									r <= "0000";
+									g <= "0000";
+									b <= "0000";
 								ELSE 
 									r <= "1111";
 									g <= "1111";
@@ -64,34 +93,8 @@ BEGIN
 								r <= "1111";
 								g <= "1111";
 								b <= "1111";
-							END IF;
-						ELSE 		
-							IF (x_map = "0101"  AND y_map = "0101") THEN
-								IF (victory_vector_2_left(spritebit) = '1') THEN
-									r <= "0000";
-									g <= "0000";
-									b <= "0000";
-								ELSE 
-									r <= "0000";
-									g <= "0000";
-									b <= "0000";
-								END IF;
-							ELSIF (x_map = "0101" AND y_map = "0101") THEN
-								IF (victory_vector_right(spritebit) = '1') THEN
-									r <= "0000";
-									g <= "0000";
-									b <= "0000";
-								ELSE 
-									r <= "0000";
-									g <= "0000";
-									b <= "0000";
-								END IF;
-							ELSE
-								r <= "0000";
-								g <= "0000";
-								b <= "0000";
-							END IF;
-						END IF;
+							END IF;	
+								
 					ELSIF (check_1 = '0') AND (check_2 = '0') THEN--explosion
 						IF (explosion_vector(spritebit) = '1') THEN
 							r <= "1111";
@@ -108,9 +111,9 @@ BEGIN
 							g <= "0000";
 							b <= "0000";
 						ELSE
-							r <= "0000";
-							g <= "1001";
-							b <= "0000";
+							r <= "0001";
+							g <= "0001";
+							b <= "0001";
 						END IF;
 					ELSIF (y_map = y_p2) AND (x_map = x_p2) THEN
 						IF (P_vector(spritebit) = '1') THEN
@@ -118,9 +121,9 @@ BEGIN
 							g <= "0000";
 							b <= "1111";
 						ELSE
-							r <= "0000";
-							g <= "1001";
-							b <= "0000";
+							r <= "0001";
+							g <= "0001";
+							b <= "0001";
 						END IF;
 					ELSIF (x_map = x_bomb_a AND y_map = y_bomb_a AND bomb_a_enable = '1') OR --bomb
 						(x_map = x_bomb_b AND y_map = y_bomb_b AND bomb_b_enable = '1') OR
@@ -135,24 +138,24 @@ BEGIN
 							g <= "0000";
 							b <= "0000";
 						ELSE
-							r <= "0000";
-							g <= "1001";
-							b <= "0000";
+							r <= "0001";
+							g <= "0001";
+							b <= "0001";
 						END IF; 
 					ELSIF (check_1 = '0') AND (check_2 = '1') THEN
 						IF (crate_vector(spritebit) = '1') THEN--crate
+							r <= "0001";
+							g <= "0101";
+							b <= "0011";
+						ELSE
 							r <= "1001";
 							g <= "0101";
 							b <= "0000";
-						ELSE
-							r <= "0011";
-							g <= "0011";
-							b <= "0000";
 						END IF; 	
 					ELSIF (check_1 = '1') AND (check_2 = '0') THEN--empty
-							r <= "0000";
-							g <= "1001";
-							b <= "0000";
+							r <= "0001";
+							g <= "0001";
+							b <= "0001";
 					
 					ELSIF (check_1 = '1') AND (check_2 = '1') THEN---must be 11 so wall
 						IF (wall_vector(spritebit) = '1') THEN
