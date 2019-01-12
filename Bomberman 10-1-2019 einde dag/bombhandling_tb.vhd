0154dec62dfec6093eaf67d0f4d4687d73df14b3
danielvanpaass <38658984+danielvanpaass@users.noreply.github.com>
2019-01-12 16:44:36 +0100

    cleanup

--- Bomberman 10-1-2019 einde dag/bombhandling_tb.vhd
+++
@@ -1,6 +0,0 @@
-library IEEE;
-use IEEE.std_logic_1164.ALL;
-
-entity bombhandling_tb is
-end bombhandling_tb;
-
--- Bomberman 10-1-2019 einde dag/uneven_bits.vhd
+++
@@ -1,8 +0,0 @@
-library IEEE;
-use IEEE.std_logic_1164.ALL;
-
-entity uneven_bits is
-   port(
-        input  : in  std_logic_vector(21 downto 0);
-        output : out std_logic_vector(10 downto 0));
-end uneven_bits;
